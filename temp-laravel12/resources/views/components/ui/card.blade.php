956073bcbbac3d347650ca3ffac1105280c585ef
prakshit <shitalmakwana2206@gmail.com>
2026-04-04 11:13:40 +0530

    remove unwanted code

--- temp-laravel12/resources/views/components/ui/card.blade.php
+++
@@ -1,6 +0,0 @@
-@props(['class' => ''])
-
-<div {{ $attributes->merge(['class' => 'rounded-3xl border border-border/60 bg-card/70 p-6 shadow-sm backdrop-blur-xl ' . $class]) }}>
-    {{ $slot }}
-</div>
-
